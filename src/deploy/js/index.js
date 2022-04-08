7ff42b8d70eb32db220440bc39217aaec0711f9c
JonathasGit <jonathas-25@hotmail.com>
2022-04-08 09:54:54 -0300

    Vendo os tipos basicos em Ts

--- src/deploy/js/index.js
+++ src/deploy/js/index.js
@@ -3,3 +3,18 @@
 let x = 10;
 x = 50;
 console.log(x);
+// inferencia x annotation = definição de variavael 
+// inferencia = menor código
+let y = 12; // Definindo a varivel por inferencia 
+let z = 12; //Definindo variavel por annotation
+// Tipo Básicos 
+let primeiroNome = "Jonathas";
+let idade = 26;
+const isAdmin = true;
+console.log(typeof primeiroNome);
+primeiroNome = "MudeiONome";
+console.log(primeiroNome);
+//objet - Definido Arrays ou lista como chama
+const myNUumbers = [1, 2, 3];
+console.log(myNUumbers);
+console.log(myNUumbers.length);
--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,26 @@ x = 50;
 
 
 console.log(x);
+
+
+// inferencia x annotation = definição de variavael 
+// inferencia = menor código
+
+let y = 12; // Definindo a varivel por inferencia 
+let z: number = 12; //Definindo variavel por annotation
+
+// Tipo Básicos 
+let primeiroNome: string = "Jonathas";
+let idade: number = 26;
+const isAdmin: boolean = true;
+
+console.log(typeof primeiroNome);
+
+primeiroNome = "MudeiONome";
+console.log(primeiroNome);
+
+//objet - Definido Arrays ou lista como chama
+const myNUumbers: number[] = [1,2,3];
+
+console.log(myNUumbers);
+console.log(myNUumbers.length);
